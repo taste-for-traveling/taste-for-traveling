923842a030c921c4f6a167f3d797e111cf9abe08
douglasio <50843229+douglasio@users.noreply.github.com>
2020-01-20 15:01:07 -0500

    Enabled sourcing from fs, created posts in md

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,13 @@ module.exports = {
     }
   },
   plugins: [
-    'gatsby-plugin-sass'
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`
+      }
+    }
   ]
 }
---
+++ src/posts/react.md
@@ -0,0 +1,6 @@
+---
+title: "My reaction to React"
+date: "2019-01-2019"
+---
+
+It is what it is.
