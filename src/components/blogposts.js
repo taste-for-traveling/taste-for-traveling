0bc447147314ccbef8d7b309ab0545a9ae282e31
douglasio <50843229+douglasio@users.noreply.github.com>
2020-01-20 16:31:03 -0500

    BlogPosts links to dyn generated post pages

--- src/components/blogposts.js
+++ src/components/blogposts.js
@@ -1,6 +1,6 @@
 import React from 'react'
+import { Link, useStaticQuery, graphql } from 'gatsby'
 import { CardColumns, Card, Badge } from 'react-bootstrap'
-import { useStaticQuery, graphql } from 'gatsby'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
 import blogPostStyles from './blogposts.module.scss'
@@ -16,8 +16,11 @@ const BlogPosts = () => {
                         date,
                         location
                     }
-                    html
-                    excerpt
+                    html,
+                    excerpt,
+                    fields {
+                        slug
+                    }
                 }
             }
             }
@@ -29,7 +32,7 @@ const BlogPosts = () => {
                 <Card key={i}>
                     <Card.Body>
                         <Card.Title as="div">
-                            <h3>{post.node.frontmatter.title}</h3>
+                            <h3><Link to={`/blog/${post.node.fields.slug}`} className={ blogPostStyles.headerLink }>{post.node.frontmatter.title}</Link></h3>
                             <Badge variant="primary">
                                 <FontAwesomeIcon icon={faMapMarkerAlt} className="mr-2" />
                                 {post.node.frontmatter.location}
@@ -37,7 +40,6 @@ const BlogPosts = () => {
                             <date className="d-block text-muted mt-1"><small>{post.node.frontmatter.date}</small></date>
                         </Card.Title>
                         <Card.Text>
-                            
                             {post.node.excerpt}
                         </Card.Text>
                     </Card.Body>
