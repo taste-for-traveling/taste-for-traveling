082157e6bb54d5ac7836807817e6f929f98bce3d
douglasio <50843229+douglasio@users.noreply.github.com>
2020-01-20 02:10:34 -0500

    Created sticky footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import Header from '../components/header'
 import Footer from '../components/footer'
+import layoutStyles from './layout.module.scss'
 
 const Layout = (props) => {
     return(
         <div>
             <Header />
-            { props.children }
-            <Footer />
+            <div className={ layoutStyles.container }> 
+                <div className={ layoutStyles.content }>
+                    { props.children }
+                </div>
+                <Footer />
+            </div>
         </div>
     )
 }
