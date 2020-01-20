27b1e7cc460a486b048abb62f9622c110666b4cd
douglasio <50843229+douglasio@users.noreply.github.com>
2020-01-20 00:03:15 -0500

    Created About and Blog pages

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BlogPage = () => {
+    return(
+        <div>
+            <h1>Blog</h1>
+            <p>Posts gon' go here.</p>
+        </div>
+    )
+}
+
+export default BlogPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
-import React from "react"
+import React from 'react'
 
-export default () => <div>Five hundredth time's the charm</div>
+const IndexPage = () => {
+    return(
+        <div>
+            <h1>Taste for Traveling</h1>
+            <h2>A taste for travel, bit by the travel bug, wanderlust&mdash;whatever you want to call it, we&rsquo;ve got it. Plus we need food to live.</h2>
+        </div>
+    )
+}
+
+export default IndexPage
