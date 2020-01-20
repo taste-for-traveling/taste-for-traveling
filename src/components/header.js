f636183fa2b6cd1d24ba49cbca6bccc69d8a45d9
douglasio <50843229+douglasio@users.noreply.github.com>
2020-01-20 01:01:51 -0500

    Added fontawesome icons for header

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Navbar, Nav } from 'react-bootstrap'
 import { Link } from 'gatsby'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
     return (
@@ -16,9 +18,9 @@ const Header = () => {
                     <Nav.Item as="nav-link"><Link to="/" className="m-3">Home</Link></Nav.Item>
                     <Nav.Item as="nav-link"><Link to="/blog" className="m-3">Blog</Link></Nav.Item>
                     <Nav.Item as="nav-link"><Link to="/about" className="m-3">About</Link></Nav.Item>
-                    <Nav.Item as="nav-link"><a href="https://www.facebook.com/TasteforTraveling/" target="_blank" className="m-3">Facebook</a></Nav.Item>
-                    <Nav.Item as="nav-link"><a href="https://www.instagram.com/tastefortraveling/" target="_blank" className="m-3">Instagram</a></Nav.Item>
-                    <Nav.Item as="nav-link"><a href="https://www.instagram.com/tastefortraveling/" target="_blank" className="m-3">YouTube</a></Nav.Item>
+                    <Nav.Item as="nav-link"><a href="https://www.facebook.com/TasteforTraveling/" target="_blank" className="m-3">Facebook <FontAwesomeIcon icon={ faChevronRight } /></a></Nav.Item>
+                    <Nav.Item as="nav-link"><a href="https://www.instagram.com/tastefortraveling/" target="_blank" className="m-3">Instagram <FontAwesomeIcon icon={ faChevronRight } /></a></Nav.Item>
+                    <Nav.Item as="nav-link"><a href="https://www.instagram.com/tastefortraveling/" target="_blank" className="m-3">YouTube <FontAwesomeIcon icon={ faChevronRight } /></a></Nav.Item>
                 </Nav>
             </Navbar>
         </header>
