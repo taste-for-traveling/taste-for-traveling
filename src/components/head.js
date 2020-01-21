2e2b87c5513d04715fcc1279bc8cb521dd2b443f
douglasio <50843229+douglasio@users.noreply.github.com>
2020-01-20 21:42:09 -0500

    Borked bootstrap

--- src/components/head.js
+++ src/components/head.js
@@ -5,7 +5,6 @@ import { Helmet } from 'react-helmet'
 import { useStaticQuery, graphql } from 'gatsby'
 
 require('jquery')
-require('bootstrap')
 
 const Head = ({ title }) => {
     const data = useStaticQuery(graphql`
@@ -17,8 +16,15 @@ const Head = ({ title }) => {
             }
         }
     `)
-    return(
-        <Helmet title={`${title} - ${data.site.siteMetadata.title}`} />
+    return(  
+        <Helmet title={`${title} - ${data.site.siteMetadata.title}`}>
+            <link
+                rel="stylesheet"
+                href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+                integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+                crossorigin="anonymous"
+                />
+        </Helmet>
     )
 }
 
