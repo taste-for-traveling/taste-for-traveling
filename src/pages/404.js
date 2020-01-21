6309c196d8b6efabb6a5d88c270300063db88187
douglasio <50843229+douglasio@users.noreply.github.com>
2020-01-20 20:25:56 -0500

    Added some styling to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,15 +3,27 @@ import React from 'react'
 //Gatsby
 import { Link } from 'gatsby'
 
+//Bootstrap
+import { Container } from 'react-bootstrap'
+
 //Components
 import Layout from '../components/layout'
 import Head from '../components/head'
 
+//Styles
+import notFoundStyles from '../styles/notfound.module.scss'
+
+export const title = "Here be dragons"
+
 const NotFound = () => {
     return(
         <Layout>
-            <Head title="Here be dragons" />
-            <p><Link to="/">Head home</Link></p>
+            <Head title={title} />
+            <Container className={notFoundStyles.container}>
+                <h1 className="display-1">Whoops...</h1>
+                <h3>Nothing to see or eat here.</h3>
+                <p><Link to="/">Back home</Link></p>
+            </Container>
         </Layout>
     )
 }
